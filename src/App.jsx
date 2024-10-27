4b27dd9e676347abde1826fe8476641d3a68949d
Satyam Kumar <satyam.kr.7188@gmail.com>
2024-10-27 22:39:07 +0530

    init push add image

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import hill1 from "/hill1.png"
+import hill2 from "/hill2.png"
+import hill3 from "/hill3.png"
+import hill4 from "/hill4.png"
+import hill5 from "/hill5.png"
+import tree from "/tree.png"
+import plant from "/plant.png"
+import leaf from "/leaf.png"
+
+function App() {
+
+  return (
+    <>
+      <img src={hill1} alt="image hill" />
+      <img src={hill2} alt="image hill2" />
+      <img src={hill3} alt="image hill3" />
+      <img src={hill4} alt="image hill4" />
+      <img src={hill5} alt="image hill5" />
+      <img src={tree} alt="image tree" />
+      <img src={plant} alt="image pllant" />
+      <img src={leaf} alt="image leaf" />
+    </>
+  );
+}
+
+export default App;
