0e8fad30fef2556c23b72bd81375499346ebbb21
Satyam Kumar <satyam.kr.7188@gmail.com>
2024-11-18 08:30:59 +0530

    added phone images

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,22 @@ import tree from "/tree.png"
 import plant from "/plant.png"
 import leaf from "/leaf.png"
 import login from "/login.png"
+import phone01 from "/phone01.jpg"
+import phone02 from "/phone02.jpg"
+import phone03 from "/phone03.jpg"
+import phone04 from "/phone04.jpg"
+import phone05 from "/phone05.jpg"
 
 function App() {
 
   return (
     <>
 
+      <img src={phone01} alt="image phone01" />
+      <img src={phone02} alt="image phone02" />
+      <img src={phone03} alt="image phone03" />
+      <img src={phone04} alt="image phone04" />
+      <img src={phone05} alt="image phone05" />
       <img src={login} alt="image login" />
       <img src={hill1} alt="image hill" />
       <img src={hill2} alt="image hill2" />
